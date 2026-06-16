14f5c735fc0be9e5ad0fb57a5a42ce7f1545fa93
Hugo <hugo.martin@supdevinci-edu.fr>
2026-06-16 10:09:46 +0200

    pki et bastion opérationel

--- bluelockin/bastion/scripts/generate_certs.py
+++ bluelockin/bastion/scripts/generate_certs.py
@@ -4,19 +4,22 @@ import requests
 
 # 1. Configuration des paramètres (Industrialisables via variables d'environnement)
 VAULT_ADDR = os.getenv("VAULT_ADDR", "http://localhost:8200")
-# Ici, remplace par le jeton généré par ta PKI globale
 VAULT_TOKEN = "root_pki_token_2026"
 
-CERTS_DIR = os.path.abspath(os.path.join(os.getcwd(), "../vault-agent/certs"))
+# CORRECTION : Forçage du chemin au format Linux natif pour s'aligner avec Docker
+# On part du dossier du script, on remonte d'un cran, puis on descend proprement
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CERTS_DIR = os.path.join(BASE_DIR, "vault-agent", "certs")
+
 CRT_PATH = os.path.join(CERTS_DIR, "bastion.crt")
 KEY_PATH = os.path.join(CERTS_DIR, "bastion.key")
 
 def generate_bastion_certificates():
     print("--- [AUTOMATISATION PKI] Demande de certificats à Vault ---")
     
-    # Sécurité : Création du dossier de destination s'il n'existe pas
+    # Sécurité : Création de la vraie structure imbriquée si elle n'existe pas
     if not os.path.exists(CERTS_DIR):
-        os.makedirs(CERTS_DIR)
+        os.makedirs(CERTS_DIR, exist_ok=True)
 
     # Préparation de la requête API pour Vault
     url = f"{VAULT_ADDR}/v1/pki/issue/bastion-role"
@@ -42,13 +45,13 @@ def generate_bastion_certificates():
         # Extraction et écriture du Certificat (Cert + CA chain)
         certificate = secret_data["certificate"]
         issuing_ca = secret_data["issuing_ca"]
-        with open(CRT_PATH, "w") as crt_file:
+        with open(CRT_PATH, "w", encoding="utf-8") as crt_file:
             crt_file.write(certificate + "\n" + issuing_ca)
         print(f"✓ Certificat sauvegardé : {CRT_PATH}")
 
         # Extraction et écriture de la Clé Privée
         private_key = secret_data["private_key"]
-        with open(KEY_PATH, "w") as key_file:
+        with open(KEY_PATH, "w", encoding="utf-8") as key_file:
             key_file.write(private_key)
         print(f"✓ Clé privée sauvegardée : {KEY_PATH}")
         
--- bluelockin/bastion/vault-agent/template/bastion.key.tpl
+++
@@ -1,3 +0,0 @@
-{{ with secret "pki/issue/bastion-role" "common_name=bastion.blue.local" "ttl=720h" }}
-{{ .Data.private_key }}
-{{ end }}
